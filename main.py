99ea29f79144fca5503a14f439455a6566b94f43
Peter Manda <109303643+PeterManda19@users.noreply.github.com>
2022-11-08 08:25:50 +0000

    Added time module and printing amount to two decimal places

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+import time
+
 print("Loan Calculator.")
 print()
 print("Shows how much money you owe for a loan of $1,000 with a 5% APR (Annual Percentage Rate) over 10 years.")
 print()
+print("Busy calculating how much you will owe after 10 years...")
+print()
+time.sleep(3)
+
 loan = 1000
 perc = 0.05
 
 for yrs in range(1,11):
   owing = loan + (loan*0.05)*yrs
-print("You owe $",owing)
+print("You owe ${:.2f}".format(owing))
 
 
 def endGame():
